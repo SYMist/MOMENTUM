6b15f3876ce5253497414e7080ff557157db6d35
Suyeon <mmist0226@gmail.com>
2022-01-04 00:08:44 +0900

    LOGIN Session

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username'
+// string만 저장된 변수는 대문자로 표기, 별로 중요하지 않은 것도 이렇게 표기
 
 function onLoginSubmit(event) {
     event.preventDefault(); 
-    // 브라우저의 기본 동작을 막아줌
-    console.log(loginInput.value);
+    // 브라우저의 기본 동작을 막아줌. 여기서 기본동작은 입력 후 새로고침되는 거
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
-// submit이라는 이벤트에 대해 onLoginSubmit이라는 함수를 실행함.
-// onLoginSubmit이라는 함수에 event라는 인자를 넣고,
-    // 인자를 아무것도 넣지 않으면, 메소드를 붙일 수 없다.  
-// 이 인자에 preventDefault()라는 메소드를 넣어 브라우저의 기본 동작을 막는다.
-    // '새로고침'이라는 기본 동작을 막지 않으면 유저의 정보가 저장된 직후 바로 보이지 않게 된다.
-// 유저가 입력한 값을 알기 위해 console.log의 인자를 loginInput.value로 넣는다.
+function paintGreetings(username) {
+    greeting.innerText = `Hello, ${username}!`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUserName = localStorage.getItem("username");
+
+if (savedUserName === null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+
+} else {
+    // show the greetings
+    paintGreetings(savedUserName);
+        // greeting.innerText = 'Hi, '+ username;
+}
