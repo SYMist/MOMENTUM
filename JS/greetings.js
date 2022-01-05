a89a273169c831d37c72ebc13368b150dc9e9f09
SYMist <mmist0226@gmail.com>
2022-01-06 00:25:52 +0900

    push

--- JS/greetings.js
+++ JS/greetings.js
@@ -11,12 +11,12 @@ function onLoginSubmit(event) {
   event.preventDefault(); 
   // 브라우저의 기본 동작을 막아줌. 여기서 기본동작은 입력 후 새로고침되는 거
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
-  localStorage.setItem(USERNAME_KEY, username);
-  paintGreetings(username);
+  localStorage.setItem(USERNAME_KEY, loginInput.value);
+  paintGreetings();
 }
 
-function paintGreetings(username) {
+function paintGreetings() {
+  const username = localStorage.getItem(USERNAME_KEY);
   greeting.innerText = `Hello, ${username}!`;
   // greeting.innerText = 'Hi, '+ username;
   greeting.classList.remove(HIDDEN_CLASSNAME);
