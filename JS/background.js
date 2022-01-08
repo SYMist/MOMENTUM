de69370fbf7338ea98652ac217fd619032e4b295
SYMist <mmist0226@gmail.com>
2022-01-09 00:57:07 +0900

    Decorating...

--- JS/background.js
+++ JS/background.js
@@ -1,10 +1,27 @@
-const images =
-  ["0.jpeg", "1.jpg", "2.jpeg", "3.jpeg", "4.jpg", "5.jpg", "6.jpeg", "7.jpg", "8.jpg", "9.jpg"];
+const body = document.querySelector('body');
 
-const chosenImage = images[Math.floor(Math.random()*images.length)]; 
+const IMAGE_NUMBER = 6;
+const IMAGE_CLASS_NAME = "bgimage"
 
-const bgImage = document.createElement("img");
+function printImage(imageNum) {
+  const image = new Image()
+  image.src = `Img/${imageNum}.jpg`
+  image.classList.add(IMAGE_CLASS_NAME);
+  body.append(image);
+};
 
-bgImage.src = `Img/${chosenImage}`;
+function genRandom() {
+  const number = Math.floor(Math.random() * IMAGE_NUMBER + 1);
+  return number;
+};
 
-document.body.append(bgImage);
+function init() {
+  const randomNumber = genRandom();
+  printImage(randomNumber);
+};
+
+init();
+
+// body.style.filter = 'brightness(50%)';
+
+// 1~6의 숫자가 필요함
