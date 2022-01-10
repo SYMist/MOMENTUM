715f8cdee8acf1eeb7d0cfe6aa3e6b247545351d
SYMist <mmist0226@gmail.com>
2022-01-11 01:34:15 +0900

    randomImage Successgit pushgit push

--- JS/album.js
+++ JS/album.js
@@ -1,16 +1,26 @@
 const IMAGE_API_KEY = '5apxtFF_QrhY7hqKSlPD0RfaawvmrCRTjm4Ffic8Oeo';
 const randomImage = document.querySelector('#random_image');
 
-
-
 function getRandomImage() {
-  const img = new Image()
+  const image = new Image();
   const url = `https://api.unsplash.com/photos/random?client_id=${IMAGE_API_KEY}`;
   fetch(url)
     .then(response => response.json())
-    .then(data => data.urls.regular)
-  img.src = url;
-  randomImage.appendChild(img);
-}
+    .then(data => image.src = data.urls.regular)
+  randomImage.appendChild(image);
+};
 
 getRandomImage();
+
+
+// 아래는 실패한 코드
+// function getRandomImage() {
+//   const img = new Image()
+//   const url = `https://api.unsplash.com/photos/random?client_id=${IMAGE_API_KEY}`;
+//   fetch(url)
+//     .then(response => response.json())
+//   img.src = url;
+//   randomImage.appendChild(img);
+// }
+
+// getRandomImage();
