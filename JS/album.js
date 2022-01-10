9f35ed03e2ea50962a8eeb759179cdefeaea9db5
SYMist <mmist0226@gmail.com>
2022-01-11 01:47:34 +0900

    220111 album

--- JS/album.js
+++ JS/album.js
@@ -2,7 +2,7 @@ const IMAGE_API_KEY = '5apxtFF_QrhY7hqKSlPD0RfaawvmrCRTjm4Ffic8Oeo';
 const randomImage = document.querySelector('#random_image');
 
 function getRandomImage() {
-  const image = new Image();
+  const image = new Image(width = 500, height = 500);
   const url = `https://api.unsplash.com/photos/random?client_id=${IMAGE_API_KEY}`;
   fetch(url)
     .then(response => response.json())
