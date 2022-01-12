db62760f6bea48c07bb5e3f1f6ed76ae2d64843b
SYMist <mmist0226@gmail.com>
2022-01-12 13:59:48 +0900

    randomImage

--- JS/album.js
+++ JS/album.js
@@ -1,18 +1,19 @@
 const IMAGE_API_KEY = '5apxtFF_QrhY7hqKSlPD0RfaawvmrCRTjm4Ffic8Oeo';
-const randomImage = document.querySelector('#random_image');
+const randomImage = document.querySelector('#image');
 
 function getRandomImage() {
-  const image = new Image(width = 500, height = 500);
+  // const image = new Image(width = 500, height = 500);
   const url = `https://api.unsplash.com/photos/random?client_id=${IMAGE_API_KEY}`;
   fetch(url)
     .then(response => response.json())
-    .then(data => image.src = data.urls.regular)
-    randomImage.appendChild(image);
+    .then(data =>
+       randomImage.src  = data.urls.regular
+      );
+    // randomImage.appendChild(image);
 };
 
 getRandomImage();
 setInterval(getRandomImage, 30 * 1000);
-
 
 
 // 아래는 실패한 코드
