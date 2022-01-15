90f239fefc031cc10f0af9cf177cc7d3877114d3
SYMist <mmist0226@gmail.com>
2022-01-15 18:39:59 +0900

    quote deco little

--- JS/quotes.js
+++ JS/quotes.js
@@ -4,7 +4,7 @@ const quotes = [
   author: 'Estee Lauder'
   },
   {
-  quote: 'Do not try to be original, just try to be good.',
+  quote: 'Do not try to be original, just try to be good',
   author: 'Paul Rand'
   },
   {
@@ -12,11 +12,11 @@ const quotes = [
   author: 'John D. Rockefeller'
   },
   {
-  quote: 'If you cannot fly then run. If you cannot run, then walk. And if you cannot walk, then crawl, but whatever you do, you have to keep moving forward.',
+  quote: 'If you cannot fly then run. If you cannot run, then walk. And if you cannot walk, then crawl, but whatever you do, you have to keep moving forward',
   author: 'Martin Luther King Jr.'
   },
   {
-  quote: 'Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.',
+  quote: 'Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time',
   author: 'Thomas Edison'
   },
   {
@@ -36,7 +36,7 @@ const quotes = [
   author: 'Seth Godin'
   },
   {
-  quote: 'If you really want to do something, you will find a way. If you do not, you will find an excuse.',
+  quote: 'If you really want to do something, you will find a way. If you do not, you will find an excuse',
   author: 'Jim Rohn'
   },
   ];
