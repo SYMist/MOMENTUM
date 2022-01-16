fb62f039327ee47dfd0a1bdb3f15bcbd5eb82caf
SYMist <mmist0226@gmail.com>
2022-01-16 19:37:01 +0900

    Improvement of todolist and adding comment

--- JS/todo.js
+++ JS/todo.js
@@ -1,69 +1,75 @@
-const toDoForm = document.querySelector('#todo-form');
-const toDoInput = toDoForm.querySelector('#todo-form input');
-const toDoList = document.querySelector('#todo-list');
+const todoForm = document.querySelector('#todo-form');
+const todoInput = todoForm.querySelector('#todo-form input');
+const todoList = document.querySelector('#todo-list');
 const deleteAllBtn = document.querySelector('#delete-all');
 
-const TODOS_KEY = "todos";
+const ITEM_KEY = "TodoItem";
+const HIDDEN = 'hidden';
 
-let toDos = [];
+let todoItemArray = [];
 
-function saveToDos() {
-  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+todoForm.addEventListener("submit", handleToDoSubmit);
+
+function saveTodoItem() {
+  localStorage.setItem(ITEM_KEY, JSON.stringify(todoItemArray));
   //JSON.stringify(a) = a를 string으로 변환한다.
 }
 
 function deleteAll() {
-  localStorage.removeItem(TODOS_KEY);
-  toDoList.remove();
+  const removeStorage = localStorage.removeItem(ITEM_KEY);
+  todoItemArray = removeStorage;
+  todoList.classList.add(HIDDEN)
 }
 
-function deleteToDo(event) {
-  const li = event.target.parentElement;
-  li.remove();
-  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
-  saveToDos();         
+function handleDeleteItem() {
+  const deleted = this.parentElement;
+  deleted.remove();
+  todoItemArray = todoItemArray.filter((toDo) => toDo.id !== parseInt(deleted.id));
+  //todoItemArray 배열에 어떤 조건에 해당하는 애들만 포함한 배열을 할당한다.(즉, true인 애들만 남긴다.) 여기서 '어떤 조건'은 '지워진 애들과 id과 같지 않음'을 의미
+  //Int로 파싱하는 이유는 deleted.id는 saveTodoItem에서 확인할 수 있듯 String으로 저장됐기 떄문에 Int인 배열 속 id와 형태를 맞춰야 한다.
+  saveTodoItem();         
 }
 // 아래로 바꿔도 똑같이 작동한다.
-// function deleteTodo() {
-//   const li = this.parentElement;
-//   li.remove();
+// function handleDeleteItem(event) {
+//   const deleted = event.target.parentElement;
+//   deleted.remove();
 // }
 
 function handleToDoSubmit(event) {
   event.preventDefault();
-  const newTodo = toDoInput.value;
-  toDoInput.value = "";
-  const newTodoObj = {
-    text: newTodo,
-    id: Date.now(),
+  const todoItemObj = {
+    text: todoInput.value,
+    id: Math.random() * 10e20,
   };
-  toDos.push(newTodoObj);
-  paintToDo(newTodoObj);
-  saveToDos();
+  todoItemArray.push(todoItemObj);
+  paintTodoItem(todoItemObj);
+  saveTodoItem();
+  todoInput.value = "";
 }
 
-function paintToDo(newTodo) {
-  const li = document.createElement('li');
-  li.id = newTodo.id;
-  const span = document.createElement('span');
-  span.innerText = newTodo.text;
-  const button = document.createElement('button');
-  button.innerText = '❌';
-  button.addEventListener('click', deleteToDo);
-  li.appendChild(span);
-  li.appendChild(button);
-  toDoList.appendChild(li);
-  // append는 맨 마지막에 있어야 함
+function paintTodoItem(item) {
+  const wholeList = document.createElement('li');
+  wholeList.id = item.id;
+  const itemDesc = document.createElement('span');
+  itemDesc.innerText = item.text;
+  const itemDeleteButton = document.createElement('button');
+  itemDeleteButton.innerText = '𝙓';
+  itemDeleteButton.addEventListener('click', handleDeleteItem);
+  wholeList.appendChild(itemDesc);
+  wholeList.appendChild(itemDeleteButton);
+  todoList.appendChild(wholeList);
 }
 
-toDoForm.addEventListener("submit", handleToDoSubmit);
 deleteAllBtn.addEventListener('click', deleteAll);
 
-const savedToDos = localStorage.getItem(TODOS_KEY);
+const savedTodoItem = localStorage.getItem(ITEM_KEY);
 
-if (savedToDos) {
-  const parsedToDos = JSON.parse(savedToDos);
+if (savedTodoItem) {
+  const parsedItems = JSON.parse(savedTodoItem);
   //JSON.parse(a) = a를 array로 변환한다.
-  toDos = parsedToDos;
-  parsedToDos.forEach(paintToDo);
+  todoItemArray = parsedItems;
+  //바꾼 애들을 array에 넣어줘야 함(할당함).
+  todoItemArray.forEach(paintTodoItem);
+  //할당된 애들은 객체의 형태를 띄고 있음.
+  //array에 paintToDo 함수를 각각 씌워준다.
 }
